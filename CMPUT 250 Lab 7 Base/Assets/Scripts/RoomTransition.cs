95ca0f512f9313819ecdf196a4fb2d0f0028b8db
Matthew Guzdial <guzdial@Matthews-MacBook-Pro.local>
2023-10-19 21:45:59 -0600

    Initial commit

---
+++ CMPUT 250 Lab 7 Base/Assets/Scripts/RoomTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomTransition : MonoBehaviour
+{
+    public string SceneToTransitionTo = "";
+
+
+    // called when the cube hits the floor
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        TopDownPlayerBehaviour player = col.gameObject.GetComponent<TopDownPlayerBehaviour>();
+
+        if(player!=null){
+
+            if(SceneToTransitionTo!=""){
+                SceneManager.LoadScene(SceneToTransitionTo);
+            }
+        }
+    }
+}
